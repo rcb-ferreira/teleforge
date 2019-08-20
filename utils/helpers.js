598f3f220a0e530b81558954871d355a0311389f
rcb-ferreira <ferreira.rcb@gmail.com>
2019-08-20 17:46:18 +0200

    refactor(helpers): create all helper functions

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,48 @@
+const _data = null;
+const next = null;
+const previous = null;
 module.exports = {
     /*
      * Node Contructor
      * @param null $data
      */
     __contructor: function (data) {
-        console.log(data);
-        return data;
-    }
+        _data = data;
+    },
 
+    /*
+     * @return mixed
+     */
+    getDate: function () {
+        return _data;
+    },
+
+    /*
+     * @return null | Node
+     */
+    getNext: function () {
+        return next;
+    },
+
+    /*
+    * @param null | Node $next
+    */
+    setNext: function (data) {
+        next = data;
+    },
+
+    /*
+    * @return null | Node
+    */
+    getPrevious: function () {
+        return previous;
+    },
+
+    /*
+    * @return null | Node
+    */
+    setPrevious: function (data) {
+        previous = data;
+    }
 
 }
